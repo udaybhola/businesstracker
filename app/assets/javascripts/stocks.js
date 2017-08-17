6912e262b9422442a0bd7c73cc235d9c80b766a4
Uday bhola <3uabhola@gmail.com>
2017-08-18 02:07:51 +0530

    Add user and stock associationand display

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,5 +1,6 @@
 var init_stock_lookup;
-init_stock_lookup=function () {
+
+init_stock_lookup = function () {
     $('#stock-lookup-form').on('ajax:before',function (event,data,status) {
         show_spinner();
     });
