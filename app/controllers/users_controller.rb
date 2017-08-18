911c39aa7bff45e759d211bc698ee54de08c1fac
Uday bhola <3uabhola@gmail.com>
2017-08-19 02:55:37 +0530

    add destroy friendship and shoe page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,8 +27,14 @@ class UsersController < ApplicationController
     if current_user.save
       redirect_to my_friends_path , notice:"Friend was succesfully added "
     else
-      redirect_to my_friends_path , flash[:error]= "There was an error with adding user as friend"
+      redirect_to my_friends_path , flash[:error] = "There was an error with adding user as friend"
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
+
   end
 
 end
